89cea31cefa463eb649ad56836aad73067aca601
Nikita <itname@bk.ru>
2026-01-28 21:30:38 +0700

    Изменена функция parse. Теперь она не знает просто Document и возвращает обычный словарь

--- src/app/parser.py
+++ src/app/parser.py
@@ -1,5 +1,7 @@
 import json
+from fastapi import HTTPException
 from app.schemas.doc import Document, UserRequest
+from pydantic import ValidationError
 
 def mrz_checksum(data):
     weights = [7, 3, 1]
@@ -21,35 +23,52 @@ def compare_doc_number(mrz_doc, req_doc):
 def compare_date(mrz_data, req_data):
     data = req_data.split('.')
     date = data[2][2:4] + data[1] + data[0]
-    return mrz_data == int(date)
+    return mrz_data == date
 
 def parse(request: UserRequest):
-    line1, line2 = request.mrz.splitlines()
+    try:
+        line1, line2 = request.mrz.splitlines()
+    except ValueError:
+        raise ValueError("MRZ должен состоять из двух строк с разделителем \\n")
 
-    # чтение первой строки
-    doc_type = line1[0:2]
-    country = line1[2:5]
+    if len(line1) < 44 or len(line2) < 44:
+        raise ValueError("Каждая строка MRZ должна содержать 44 символа")
+
+
     names = line1[5:44].strip('<').split('<<')
-    last_name = names[0]
-    first_name = names[1]
-    middle_name = names[2]
+    if len(names) < 2:
+        raise ValueError("Неверный формат ФИО в MRZ")
 
-    #чтение второй строки
-    doc_number = line2[0:10]
-    doc_check = line2[10]
-    nationality = line2[11:14]
-    birth_date = line2[14:20]
-    birth_date_check = line2[20]
-    sex = line2[21]
-    expiry_date = line2[22:28]
-    expiry_date_check = line2[28]
-    personal_code = line2[29:42]
-    personal_code = personal_code.replace('<', '0')
-    personal_code_check = line2[42]
-    end_check = line2[43]
-    return Document(
-        doc_type=doc_type, country=country, first_name=first_name, last_name=last_name, middle_name=middle_name,
-        doc_number=doc_number, doc_number_check=doc_check, nationality=nationality, birth_date=birth_date,
-        birth_date_check=birth_date_check, sex=sex, expiry_date=expiry_date, expiry_date_check=expiry_date_check,
-        personal_code=personal_code, personal_code_check=personal_code_check, end_check=end_check
-    )
+    return {
+        "doc_type": line1[0:2],
+        "country": line1[2:5],
+        "last_name": names[0],
+        "first_name": names[1],
+        "middle_name": names[2] if len(names) > 2 else None,
+        "doc_number": line2[0:10],
+        "doc_number_check": line2[10],
+        "nationality": line2[11:14],
+        "birth_date": line2[14:20],
+        "birth_date_check": line2[20],
+        "sex": line2[21],
+        "expiry_date": line2[22:28],
+        "expiry_date_check": line2[28],
+        "personal_code": line2[29:42].replace('<', '0'),
+        "personal_code_check": line2[42],
+        "end_check": line2[43]
+    }
+
+def build_document(data: dict) -> Document:
+    try:
+        return Document(**data)
+    except ValidationError as e:
+        raise HTTPException(
+            status_code=422,
+            detail=[
+                {
+                    'field': '.'.join(map(str, err['loc'])),
+                    'message': err['msg']
+                }
+                for err in e.errors()
+            ]
+        )
