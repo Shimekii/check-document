32d93aee79cd763139393eaf6f081a98b5ae9984
Nikita <itname@bk.ru>
2026-01-30 14:22:19 +0700

    Добавлен mock для Redis

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,11 +1,19 @@
 import httpx
 import pytest
 from httpx import ASGITransport
-
+from tests.mocks.FakeRedis import FakeRedis
+from app.dependencies.redis import get_redis
 from app.main import app
 
 @pytest.fixture
-async def async_client():
+async def fake_redis():
+    redis = FakeRedis()
+    yield redis
+
+
+@pytest.fixture
+async def async_client(fake_redis):
+    app.dependency_overrides[get_redis] = lambda: fake_redis
     transport = ASGITransport(app=app)
     async with httpx.AsyncClient(transport=transport, base_url='http://test') as client:
         yield client
